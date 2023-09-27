058fde27e69d21859c247d14bc4cadcc3368a7c8
Aydot <ayomidotzee@gmail.com>
2023-09-27 03:26:51 -0700

    Finished testimonial section

--- client/src/components/HomeComponents/Testimonials.tsx
+++ client/src/components/HomeComponents/Testimonials.tsx
@@ -11,19 +11,32 @@ export default function Testimonials() {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        cssEase: "linear"
+        cssEase: "linear",
+        arrows: false
     }
 
     return (
-        <section className="px-8 md:px-10 lg:px-20 mt-[80px]">
-            <h2 className="text-center font-Lora text-[1.5rem]">Customer Reviews</h2>
+        <section className="px-8 md:px-10 lg:px-20 mt-[80px] bg-[url('/testimonialBackground.jpg')] bg-cover bg-center md:bg-fixed py-6">
+            <h2 className="text-center font-Lora text-[1.5rem] text-black/75 font-semibold">Customer Reviews</h2>
             <div className="mt-8">
                 <Slider {...settings}>
-                    <div className="h-[200px] w-[200px] bg-blue-300">
-                        <h3>1</h3>
+                    <div className="">
+                        <div className="border-[1px] border-[#808080] p-8">
+                            <p className="text-[1.2rem] text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nulla quis optio harum illum. Beatae debitis error alias molestias. Perferendis provident rerum ea aperiam quis fugiat sit, optio labore. Praesentium.</p>
+                        </div>
+                        <div className="flex flex-col items-center justify-center mt-8 text-white/75">
+                            <h3 className="font-semibold text-[1.2rem]">Tife Enoch</h3>
+                            <p>Model</p>
+                        </div>
                     </div>
-                    <div className="h-[200px] w-[200px] bg-blue-300">
-                        <h3>2</h3>
+                    <div className="">
+                        <div className="border-[1px] border-[#808080] p-8">
+                            <p className="text-[1.2rem] text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nulla quis optio harum illum. Beatae debitis error alias molestias. Perferendis provident rerum ea aperiam quis fugiat sit, optio labore. Praesentium.</p>
+                        </div>
+                        <div className="flex flex-col items-center justify-center mt-8 text-white/75">
+                            <h3 className="font-semibold text-[1.2rem]">Triple Odunayo</h3>
+                            <p>Stakeholder</p>
+                        </div>
                     </div>
                 </Slider>
             </div>
